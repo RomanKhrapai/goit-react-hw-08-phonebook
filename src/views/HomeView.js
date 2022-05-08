65253a50436d80386b1d407fe4b7747b64fd81ec
RomanKhrapai <hrapayr@gmail.com>
2022-05-08 22:09:37 +0300

    +

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
 export default function HomeView() {
   return (
     <>
       <h1> HOME</h1>
-      <Outlet />
     </>
   );
 }
