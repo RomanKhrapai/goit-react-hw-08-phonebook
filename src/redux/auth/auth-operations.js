63380c95ba9bf473c0fa8392fe95f3d0af62b310
RomanKhrapai <hrapayr@gmail.com>
2022-05-10 22:17:55 +0300

    +

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -131,7 +131,7 @@ const fetchCurrentUser = createAsyncThunk(
       error => {
         return error;
       };
-      return thunkAPI.rejectWithValue('Unknown error!!!');
+      return thunkAPI.rejectWithValue('Error Token !!!');
     }
   }
 );
