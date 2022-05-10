f3b406c18e20124ebc49e078fd596f41f39f04c9
RomanKhrapai <hrapayr@gmail.com>
2022-05-10 21:02:51 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -34,7 +34,7 @@ export const store = configureStore({
   },
   middleware,
   // eslint-disable-next-line no-undef
-  devTools: process.env.NODE_ENV === 'development',
+  //devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
